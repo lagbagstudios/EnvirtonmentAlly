cfc27332221613cf509a685452421786f7d70e2c
Anders Olson <sredna43@gmail.com>
2023-10-26 11:27:46 -0500

    Barebones of movement

--- scenes/characters/main_character/scripts/main_character.gd
+++ scenes/characters/main_character/scripts/main_character.gd
@@ -1,2 +1,13 @@
-extends Node2D
+extends CharacterBody2D
 class_name Player
+
+@export var movement_speed = 10.0
+
+@onready var state_machine: StateMachine = $States
+
+func move(direction: Vector2) -> void:
+	velocity = direction * movement_speed
+	move_and_slide()
+
+func _physics_process(delta):
+	state_machine.run(delta)
--- scenes/characters/main_character/scripts/player_state.gd
+++ scenes/characters/main_character/scripts/player_state.gd
@@ -2,3 +2,8 @@ extends State
 class_name PlayerState
 
 var player: Player
+
+func _ready() -> void:
+	await(owner.ready)
+	player = owner as Player
+	assert(player != null)
